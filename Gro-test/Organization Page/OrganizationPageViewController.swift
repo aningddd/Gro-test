d4d422466ec5597ef54edf575b31e6be8ac71c6e
Long Do <longhoangdo16@gmail.com>
2022-10-18 12:46:09 -0500

    add folder

---
+++ Gro-test/Organization Page/OrganizationPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OrganizationPageViewController.swift
+//  Gro-test
+//
+//  Created by Long Do on 10/18/22.
+//  Copyright © 2022 Shu Tong Luo. All rights reserved.
+//
+
+import UIKit
+
+class OrganizationPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
