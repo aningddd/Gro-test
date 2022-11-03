9b31d2c5724ac38f94bd4f2bb6684da245e08a40
Christopher Chen <chris.chen23@utexas.edu>
2022-11-03 18:14:11 -0500

    adding firebase authentication for user login, editing a few segues

--- Gro-test/Register Login1/LoginVC.swift
+++ Gro-test/Register Login1/LoginVC.swift
@@ -7,15 +7,46 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginVC: UIViewController {
 
     @IBOutlet weak var LoginTitleLabel: UILabel!
+    @IBOutlet weak var emailField: UnderlineTextField!
+    @IBOutlet weak var passwordField: UnderlineTextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var errorMessage: UILabel!
+    let loginSegue = "loginSegueIdentifier"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        errorMessage.isHidden = true
+        passwordField.isSecureTextEntry = true
         navigationController?.setNavigationBarHidden(false, animated: false)
         LoginTitleLabel.font = UIFont(name:"ArialRoundedMTBold",size:30.0)
     }
-
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        //handling the case to login the user to the app
+        print("Got here")
+        Auth.auth().signIn(withEmail: emailField.text!, password: passwordField.text!){
+            authResult, error in
+            self.errorMessage.isHidden = false
+            if let error = error as NSError?{
+                self.errorMessage.text = "\(error.localizedDescription)"
+            }
+            else{
+                self.errorMessage.text = "Success"
+                Auth.auth().addStateDidChangeListener(){
+                    auth, user in
+                    print(user!)
+                    if user != nil{
+                        self.performSegue(withIdentifier: self.loginSegue, sender: nil)
+                        self.emailField.text = nil
+                        self.passwordField.text = nil
+                    }
+                }
+            }
+        }
+    }
+    
 }
--- Gro-test/Register Login1/RegisterPageVC.swift
+++ Gro-test/Register Login1/RegisterPageVC.swift
@@ -7,30 +7,48 @@
 //
 
 import UIKit
+import FirebaseAuth
 
-let registerContent = ["Email", "Password","Re-enter Password", "Username"]
 
-class RegisterPageVC: UIViewController,UITableViewDelegate, UITableViewDataSource {
+class RegisterPageVC: UIViewController {
     
     @IBOutlet weak var registerPageTitleLabel: UILabel!
-    @IBOutlet weak var mainTable: UITableView!
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return registerContent.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "registerTC", for: indexPath) as! LabelTextFieldCell
-        cell.titleLabelForCell.text = registerContent[indexPath.row]
-        
-        return cell
-    }
+    @IBOutlet weak var emailAddressLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var confirmPasswordLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var confirmPasswordField: UITextField!
+    @IBOutlet weak var errorMessage: UILabel!
+    @IBOutlet weak var firstNameField: UITextField!
+    @IBOutlet weak var lastNameField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        mainTable.delegate = self
-        mainTable.dataSource = self
+        passwordField.isSecureTextEntry = true
+        confirmPasswordField.isSecureTextEntry = true
+        errorMessage.isHidden = true
         registerPageTitleLabel.font = UIFont(name:"ArialRoundedMTBold",size:30.0)
     }
-
+    //create the new account with the user login credentials
+    @IBAction func createAccountButtonPressed(_ sender: Any) {
+        if(passwordField.text == confirmPasswordField.text){
+            Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordField.text!){
+                authResult, error in
+                self.errorMessage.isHidden = false
+                if let error = error as NSError?{
+                    self.errorMessage.text = "\(error.localizedDescription)"
+                }
+                else{
+                    self.errorMessage.text = "Success"
+                }
+            }
+        }
+        else{
+            self.errorMessage.isHidden = false
+            self.errorMessage.text = "Error: passwords do not match"
+        }
+    }
+    
 }
