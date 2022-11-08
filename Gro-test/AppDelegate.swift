0de8ddfb677d038393e3940f89ed3e6f4f18edbb
bwn133 <bowen.yang18@qq.com>
2022-11-08 12:37:14 -0600

    backend tested

--- Gro-test/AppDelegate.swift
+++ Gro-test/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        DataManager.app.UploadUserData(userName: "test1", orgAvatar: UIImage(named: "Image")!, orgDescription: "none", type: "orgData")
         return true
     }
 
