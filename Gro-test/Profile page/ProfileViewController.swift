2b10a9ad12c9205d01ebf63710b149b59f1d6a85
Christopher Chen <chris.chen23@utexas.edu>
2022-11-08 20:28:06 -0600

    adding profile picture change functionality

--- Gro-test/Profile page/ProfileViewController.swift
+++ Gro-test/Profile page/ProfileViewController.swift
@@ -16,6 +16,7 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var userEmailLabel: UILabel!
     @IBOutlet weak var settingsButton: UIButton!
     @IBOutlet weak var switchAccountButton: UIButton!
+    @IBOutlet weak var changeProfilePictureButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +27,70 @@ class ProfileViewController: UIViewController {
         profileImage.layer.cornerRadius = 85
     }
     
+    @IBAction func pressedProfilePictureButton(_ sender: Any) {
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        
+        let controller = UIAlertController(
+            title: "Photo Source",
+            message: "Choose a source",
+            preferredStyle: .actionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .cancel,
+                                         handler: {
+            (action) in print("Cancel action")
+        })
+        controller.addAction(cancelAction)
+    
+        let OKAction = UIAlertAction(title: "Choose from photo library",
+                                     style: .default,
+                                     handler: {
+            (action) in
+            vc.sourceType = .photoLibrary
+            self.present(vc, animated: true)
+        })
+        controller.addAction(OKAction)
+        
+        let cameraAction = UIAlertAction(title: "Take photo with camera",
+                                     style: .default,
+                                     handler: {
+            (action) in
+            if(UIImagePickerController.isSourceTypeAvailable(.camera )){
+                vc.sourceType = .camera
+                self.present(vc, animated: true)
+            }
+            else{
+                let cameraAlertController = UIAlertController(
+                    title: "Camera not available",
+                    message: "Unable to locate camera for this device",
+                    preferredStyle: .alert)
+                cameraAlertController.addAction(UIAlertAction(title: "OK",
+                                                   style: .default))
+                self.present(cameraAlertController, animated: true)
+            }
+            
+        })
+        controller.addAction(cameraAction)
+        
+        present(controller,animated:true)
+    }
     @IBAction func switchAccount(_ sender: Any) { self.navigationController?.navigationController?.popToRootViewController(animated: true)
     }
     
+}
+
+extension ProfileViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")] as? UIImage{
+            profileImage.image = image
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
 }
