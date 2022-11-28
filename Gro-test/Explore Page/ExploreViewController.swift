e376935023c4bcbd0ae89b76f99728261b356711
Christopher Chen <chris.chen23@utexas.edu>
2022-11-28 16:47:36 -0600

    adding basic explore page functionality

--- Gro-test/Explore Page/ExploreViewController.swift
+++ Gro-test/Explore Page/ExploreViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 let categories = ["Academic", "Greek Life", "Volunteering", "Sports", "Culture"]
 
-class ExploreViewController: UIViewController {
+class ResultsVC : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
+
+class ExploreViewController: UIViewController, UISearchResultsUpdating {
+    
     
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var categoryView: UIView!
     @IBOutlet weak var plusButton: UIButton!
+    
+    let searchController = UISearchController(searchResultsController: ResultsVC())
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +32,16 @@ class ExploreViewController: UIViewController {
         categoryView.layer.cornerRadius = 5
         plusButton.setImage(UIImage(systemName:"plus")?.withRenderingMode(.alwaysTemplate), for: .normal)
         plusButton.tintColor = .black
+        searchController.searchResultsUpdater = self
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let text = searchController.searchBar.text else{
+            return
+        }
+        let vc = searchController.searchResultsController as? ResultsVC
+        vc?.view.backgroundColor = .yellow
+        print(text)
     }
     
 }
