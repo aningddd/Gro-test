0ee2959c4683d143351bf125aa7ddcd23352556e
Christopher Chen <chris.chen23@utexas.edu>
2022-11-29 06:58:21 -0600

    basic explore page functionality

--- Gro-test/Explore Page/ExploreViewController.swift
+++ Gro-test/Explore Page/ExploreViewController.swift
@@ -17,31 +17,56 @@ class ResultsVC : UIViewController {
     }
 }
 
-class ExploreViewController: UIViewController, UISearchResultsUpdating {
+class ExploreViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
     
     
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var categoryView: UIView!
     @IBOutlet weak var plusButton: UIButton!
+    var filtered:[String] = []
+    var filteredData: [String]!
     
-    let searchController = UISearchController(searchResultsController: ResultsVC())
+    //var allOrgs:[String] = []
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.delegate = self
         searchBar.layer.cornerRadius = 5
         categoryView.layer.cornerRadius = 5
         plusButton.setImage(UIImage(systemName:"plus")?.withRenderingMode(.alwaysTemplate), for: .normal)
         plusButton.tintColor = .black
-        searchController.searchResultsUpdater = self
+        tableView.dataSource = self
+        searchBar.delegate = self
+        filteredData = allOrgs
     }
     
-    func updateSearchResults(for searchController: UISearchController) {
-        guard let text = searchController.searchBar.text else{
-            return
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableCell", for: indexPath) as UITableViewCell
+        cell.textLabel?.text = filteredData[indexPath.row]
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredData.count
+    }
+
+    // This method updates filteredData based on the text in the Search Box
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // When there is no text, filteredData is the same as the original data
+        // When user has entered text into the search box
+        // Use the filter method to iterate over all items in the data array
+        // For each item, return true if the item should be included and false if the
+        // item should NOT be included
+        filteredData = searchText.isEmpty ? allOrgs : allOrgs.filter { (item: String) -> Bool in
+            // If dataItem matches the searchText, return true to include it
+            return item.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
         }
-        let vc = searchController.searchResultsController as? ResultsVC
-        vc?.view.backgroundColor = .yellow
-        print(text)
+        
+        tableView.reloadData()
     }
-    
+
 }
+
